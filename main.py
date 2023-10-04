a762781343f8d9612ff13685f9247f2a32bc70d1
Andreas Celada <andreas.sch.celada@gmail.com>
2023-10-04 01:22:38 -0300

    Requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from solver import LBMFlowSolver
 import jax
 
 if __name__ == '__main__':
-    #jax.config.update("jax_enable_x64", True)
-    #solver = LBMFlowSolver()
-    #solver.run_simulation()
-    print(analysis.compare_entrance_length('data.hdf5', 'vel_data',0.001,50,1000,0.001))
+    jax.config.update("jax_enable_x64", True)
+    solver = LBMFlowSolver()
+    solver.run_simulation()
+    #print(analysis.compare_entrance_length('data.hdf5', 'vel_data',0.001,50,1000,0.001))
