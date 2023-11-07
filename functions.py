d437f50e0e28a346463cc9a71bd711babb9f6812
Andreas Celada <andreas.sch.celada@gmail.com>
2023-11-07 17:27:44 -0300

    removendo codigo desnecessario

--- functions.py
+++ functions.py
@@ -10,22 +10,12 @@ def collide(solver, discrete_velocities_prev, equilibrium_discrete_velocities):
             - solver.RELAXATION_OMEGA * (discrete_velocities_prev - equilibrium_discrete_velocities)
     )
 
-    # Bounce Back no-slip On top and bottom
+    # Bounce Back
     for i in range(solver.N_DISCRETE_VELOCITIES):
-        # discrete_velocities_post_collision = discrete_velocities_post_collision.at[:, 0,
-        #                                      solver.LATTICE_INDICES[i]].set(
-        #     discrete_velocities_prev[:, 0, solver.OPPOSITE_LATTICE_INDICES[i]]
-        # )
-        # discrete_velocities_post_collision = discrete_velocities_post_collision.at[:, -1,
-        #                                      solver.LATTICE_INDICES[i]].set(
-        #     discrete_velocities_prev[:, 0, solver.OPPOSITE_LATTICE_INDICES[i]]
-        # )
-
-        if solver.isPorous:
-            discrete_velocities_post_collision = \
-                discrete_velocities_post_collision.at[solver.mask, solver.LATTICE_INDICES[i]].set(
-                    discrete_velocities_prev[solver.mask, solver.OPPOSITE_LATTICE_INDICES[i]]
-                )
+        discrete_velocities_post_collision = \
+            discrete_velocities_post_collision.at[solver.mask, solver.LATTICE_INDICES[i]].set(
+                discrete_velocities_prev[solver.mask, solver.OPPOSITE_LATTICE_INDICES[i]]
+            )
 
     return discrete_velocities_post_collision
 
@@ -51,31 +41,14 @@ def stream(solver, discrete_velocities_post_collision):
 
 @jax.jit
 def update(solver, discrete_velocities_prev):
-    # # Prescribe outflow on right boundary null-gradient
-    # discrete_velocities_prev = discrete_velocities_prev.at[-1, :, solver.LEFT_VELOCITIES].set(
-    #     discrete_velocities_prev[-2, :, solver.LEFT_VELOCITIES]
-    # )
 
     # Calculate macroscopic velocities
     density_prev = solver.get_density(discrete_velocities_prev)
     macroscopic_velocities_prev = solver.get_macroscopic_velocities(discrete_velocities_prev, density_prev)
     macroscopic_velocities_prev = macroscopic_velocities_prev.at[:,:,0].add(solver.ACCELERATION_MASK)
-    # # Inflow Zou/He
-    # macroscopic_velocities_prev = macroscopic_velocities_prev.at[0, 1:-1, :].set(solver.velocity_profile[0, 1:-1, :])
-    # density_prev = density_prev.at[0, :].set(
-    #     (
-    #             solver.get_density(discrete_velocities_prev[0, :, solver.PURE_VERTICAL_VELOCITIES].T)
-    #             + 2 * solver.get_density(discrete_velocities_prev[0, :, solver.LEFT_VELOCITIES].T)
-    #     ) / (1 - macroscopic_velocities_prev[0, :, 0])
-    # )
 
     # Calculate equilibrium
     equilibrium_discrete_velocities = solver.get_equilibrium_velocities(macroscopic_velocities_prev, density_prev)
-
-    # # Zou/he
-    # discrete_velocities_prev = discrete_velocities_prev.at[0, :, solver.RIGHT_VELOCITIES].set(
-    #     equilibrium_discrete_velocities[0, :, solver.RIGHT_VELOCITIES]
-    # )
 
     # Collision
     discrete_velocities_post_collision = collide(solver, discrete_velocities_prev, equilibrium_discrete_velocities)
